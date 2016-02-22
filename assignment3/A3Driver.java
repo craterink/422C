90613428248cb8854457906d160cf0bf75676fd2
Cooper Raterink <craterink@utexas.edu>
2016-02-21 23:17:14 -0600

    Commented

--- assignment3/A3Driver.java
+++ assignment3/A3Driver.java
@@ -11,14 +11,26 @@ import bank.Bank;
 import bank.Transaction;
 import bank.errors.IllegalTransactionException;
 
+/**
+ * Driver for EE 422C Assignment 3
+ * @author Cooper Raterink, Aria Pahlavan
+ * EIDs: cdr2678, ******
+ * Lab section: Friday 2-3:30pm
+ */
 public class A3Driver 
 	{
 
+	/**
+	 * Performs all transactions from the input file and then prints a "receipt"
+	 * representing the final shopping cart state.
+	 * @param args Transaction file path/name is first argument.
+	 */
 	  public static void main(String[] args) 
 	  {
-		  FileReader freader = new FileReader(args[0]);//args[0]);
+		  FileReader freader = new FileReader(args[0]);
 		  BufferedReader reader = new BufferedReader(freader);
 		  ShoppingCart cart = new ShoppingCart();
+		  //Read one line at a time, parse, then perform on cart
 		  for (String line = reader.readLine(); line != null; line = reader.readLine()) 
 		  {
 				System.out.println(cart.performTransaction(new Transaction(line)));
--- shoppingcart/Transaction.java
+++ shoppingcart/Transaction.java
@@ -2,20 +2,64 @@ package shoppingcart;
 
 import shoppingcart.errors.InvalidTransactionException;
 
+/**
+ * Class that represents a transaction for the shopping cart to handle.
+ * Could be an insert, delete, etc.
+ * @author Cooper
+ *
+ */
 public class Transaction {
 
+	/**
+	 * Regex matching an insert transaction
+	 */
 	private static final String INSERT_REGEX = 
 			"insert (clothing|electronics|groceries) [a-z0-9]+ [0-9]+(.[0-9]{1,2})?"
 			+ " [0-9]+ [0-9]+ ?(N?F)?(N?P)? ?";
+	
+	/**
+	 * Regex matching a delete transaction
+	 */
 	private static final String DELETE_REGEX = "delete [a-z0-9]+ ?";
+	
+	/**
+	 * Regex matching a search transaction
+	 */
 	private static String SEARCH_REGEX = "search [a-z0-9]+ ?";
+	
+	/**
+	 * Regex matching an update transaction
+	 */
 	private static String UPDATE_REGEX = "update [a-z0-9]+ [0-9]+ ?";
+	
+	/**
+	 * Regex matching a print transaction
+	 */
 	private static String PRINT_REGEX = "print ?";
 	
-	private char transactionType = 'P';
+	/**
+	 * Char representing type of transaction:
+	 * 0 if invalid
+	 * I for insert
+	 * D for delete
+	 * S for search
+	 * P for print
+	 * U for update
+	 */
+	private char transactionType = 0;
 	
+	/**
+	 * Contains any needed information to process the transaction.
+	 */
 	private PurchaseItem transactionItem;
 	
+	/**
+	 * Initializes this transaction object with its appropriate type and item 
+	 * information while checking for validity.
+	 * @param transactionStr Input string representing a transaction
+	 * the shopping cart can perform
+	 * @throws InvalidTransactionException When an invalid exception was entered
+	 */
 	public Transaction(String transactionStr) throws InvalidTransactionException {
 		//get rid of any whitespace that isn't a single space
 		transactionStr.replaceAll("[\\t ]+", " ");
@@ -64,26 +108,46 @@ public class Transaction {
 		}
 	}
 
+	/**
+	 * Parses a print transaction
+	 * @param printStr Transaction input string
+	 */
 	private void parsePrint(String printStr) {
 		transactionType = 'P';
 	}
 
+	/**
+	 * Parses an update transaction
+	 * @param updateStr Transaction input string
+	 */
 	private void parseUpdate(String updateStr) {
 		transactionType = 'U';
 		transactionItem = new PurchaseItem(
 				updateStr.split(" ")[1], Integer.parseInt(updateStr.split(" ")[2]));
 	}
 
+	/**
+	 * Parses a search transaction
+	 * @param searchStr Transaction input string
+	 */
 	private void parseSearch(String searchStr) {
 		transactionType = 'S';
 		transactionItem = new PurchaseItem(searchStr.split(" ")[1], 0);
 	}
 
+	/**
+	 * Parses a delete transaction
+	 * @param deleteStr Transaction input string
+	 */
 	private void parseDelete(String deleteStr) {
 		transactionType = 'D';
 		transactionItem = new PurchaseItem(deleteStr.split(" ")[1], 0);
 	}
 
+	/**
+	 * Parses an insert transaction
+	 * @param insertStr Transaction input string
+	 */
 	private void parseInsert(String insertStr) {
 		transactionType = 'I';
 		String[] splitInsert = insertStr.split(" ");
